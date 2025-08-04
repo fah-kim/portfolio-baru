07f35385d10352f882c5092f23dd87726082518b
fahkim <fahrishakim4646@gmail.com>
2025-08-04 14:08:48 +0700

    Pertemuan ke-4

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\itemController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function () {
+    return "<h1>hello world!</h1>";
+});
+
+Route::resource('item', itemController::class);
