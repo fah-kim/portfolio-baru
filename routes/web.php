4798a8abdbc3bb9d2c60b70d1fe09187ffe294e0
fahkim <fahrishakim4646@gmail.com>
2025-08-12 08:41:32 +0700

    Membuat CRUD Sederhana

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,14 @@
 <?php
 
+use App\Http\Controllers\categoryController;
 use App\Http\Controllers\itemController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [itemController::class, 'index']);
 
 Route::get('/hello', function () {
     return "<h1>hello world!</h1>";
 });
 
-Route::resource('item', itemController::class);
+Route::resource('items', itemController::class);
+
+Route::resource('categories', categoryController::class);
